6fadcb74f3f26b916d74a1a22a36f6f4d6922139
Harry IM <Harry.Im94@gmail.com>
2022-04-24 15:59:29 -0400

    shareme

---
+++ shareme_frontend/src/components/CreatePin.jsx
@@ -0,0 +1,102 @@
+import React,  { useState } from 'react'
+import { AiOutlineCloudUpload } from 'react-icons/ai'
+import {MdDelete } from 'react-icons/md'
+import {useNavigate} from 'react-router-dom'
+
+import {client} from './client'
+import Spinner from './Spinner'
+import { categories } from '../utils/data'
+// categories [{ name: 'sports',  image: ''}]
+
+const CreatePin = ({ user }) => {
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [destination, setDestination] = useState();
+  const [fields, setFields] = useState();
+  const [category, setCategory] = useState();
+  const [imageAsset, setImageAsset] = useState();
+  const [wrongImageType, setWrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+
+  const uploadImage = (e) => {
+    const selectedFile = e.target.files[0];
+    // uploading asset to sanity
+    if (selectedFile.type === 'image/png' || selectedFile.type === 'image/svg' || selectedFile.type === 'image/jpeg' || selectedFile.type === 'image/gif' || selectedFile.type === 'image/tiff') {
+      setWrongImageType(false);
+      setLoading(true);
+      client.assets
+        .upload('image', selectedFile, { contentType: selectedFile.type, filename: selectedFile.name })
+        .then((document) => {
+          setImageAsset(document);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log('Upload failed:', error.message);
+        });
+    } else {
+      setLoading(false);
+      setWrongImageType(true);
+    }
+  };
+
+  return (
+    <div className = "flex flex-col justify-center items-center mt-5 lg:h-4/5">
+      {fields && (
+        <p className = "test-red-500 mb-5 text-xl transition-all duration-150 ease-in">Please fill the all the fields</p>
+      )}
+      <div className = "flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5 w-full">
+        <div className = "bg-secondaryColor p-3 flex flex-0.7 w-full">
+        <div className = "flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
+          {loading && <Spinner />}
+          {wrongImageType && <p>Wrong image type</p>}
+          {!imageAsset ? (
+            <label>
+              <div className = "flex flex-col items-center justify-center h-full">
+              <div className = "flex flex-col justify-center items-center">
+                <p className = "font-bold text-2xl">
+                <AiOutlineCloudUpload />
+                </p>
+              <p className = "text-lg">Click to upload</p>
+              </div>
+              <p className = "mt-32 text-gray-400">
+              Use high-quality JPG, SVG, PNG, GIF or TIFF less than 20 MB
+              </p>
+              </div>
+              <input
+                type="file"
+                name="upload-image"
+                onChange={uploadImage}
+                className="w-0 h-0"
+              
+                          
+              />
+
+
+            </label>
+          ) : (
+            <div className = "relative h-full">
+              <img src={imageAsset?.url} alt="uploaded-pic" className="h-full w-full"/>
+              <button
+                type="button"
+                className="absolute bottom-3 right-3 p-3 rounded-full bg-white text-xl cursor-pointer outline-none hover:shadow-md transition-all duration-500 ease-in-out"
+                onClick = {() => setImageAsset(null)}
+      
+      
+      >
+              <MdDelete />
+            </button>
+              </div>
+
+          )}
+        </div>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default CreatePin
